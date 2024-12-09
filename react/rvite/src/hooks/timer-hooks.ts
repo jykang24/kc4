bbe5908500be5052462d49647e9848e5b00b6df2
jykang24 <ttochi0105@naver.com>
2024-12-09 11:32:22 +0900

    useDebounce, useTimer추가

--- react/rvite/src/hooks/timer-hooks.ts
+++ react/rvite/src/hooks/timer-hooks.ts
@@ -1,6 +1,51 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 
-export const useTimeout = (cb: () => void, delay: number) => {
+type TimerFn = typeof setTimeout | typeof setInterval;
+type ClearFn = typeof clearTimeout | typeof clearInterval;
+
+function useTimer<T extends (...args: Parameters<T>) => ReturnType<T>>(
+  this: { timerFn: TimerFn; clearFn: ClearFn },
+  cb: T,
+  delay: number,
+  ...args: Parameters<T>
+) {
+  const cbRef = useRef(cb);
+  const argsRef = useRef(args);
+  const timerRef = useRef<ReturnType<typeof this.timerFn>>();
+
+  const { timerFn, clearFn } = this;
+  const setup = useCallback(() => {
+    timerRef.current = timerFn(cbRef.current, delay, ...argsRef.current);
+  }, [delay, timerFn]);
+  const clear = useCallback(() => clearFn(timerRef.current), [clearFn]);
+  const reset = useCallback(() => {
+    clear();
+    setup();
+  }, [clear, setup]);
+
+  useEffect(() => {
+    setup();
+    return clear;
+  }, [setup, clear]);
+
+  return { reset, clear };
+}
+
+export const useDebounce = <T extends (...args: unknown[]) => ReturnType<T>>(
+  cb: T,
+  delay: number,
+  depArr: unknown[] = []
+) => {
+  const { reset } = useTimeout(cb, delay);
+  useEffect(reset, [...depArr, delay]);
+};
+
+export const useTimeout = useTimer.bind({
+  timerFn: setTimeout,
+  clearFn: clearTimeout,
+});
+
+export const useTimeout2 = (cb: () => void, delay: number) => {
   useEffect(() => {
     const intl = setTimeout(cb, delay);
     return () => clearTimeout(intl);
@@ -15,10 +60,10 @@ export const useInterval = <T extends unknown[]>(
   //5번 돌고 멈추려면??
   useEffect(() => {
     let runCnt = 0;
-    const intl = setInterval((...args: T) => {
-      cb(...args);
-      if (runCnt > 5) clearInterval(intl);
+    const intl = setInterval(() => {
       runCnt += 1;
+      if (runCnt >= 5) clearInterval(intl);
+      cb(...args);
     }, delay);
 
     return () => clearInterval(intl);
