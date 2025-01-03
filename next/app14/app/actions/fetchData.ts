cefa5c03e33c5d3a95b304cec24e11387b44fc05
jykang24 <ttochi0105@naver.com>
2025-01-03 18:25:26 +0900

    og tag 파싱기능 추가

--- next/app14/app/actions/fetchData.ts
+++ next/app14/app/actions/fetchData.ts
@@ -1,7 +1,23 @@
 'use server';
 
+import parse from 'node-html-parser';
+
 export async function fetchData(url: string) {
   console.log('fetch Data from this url>>', url);
-  const data = await fetch(url).then((res) => res.text());
-  return data;
+  const html = await fetch(url).then((res) => res.text());
+
+  // node-html-parser로 파싱
+  const root = parse(html);
+  const title =
+    root.querySelector('meta[property="og:title"]')?.getAttribute('content') ||
+    'no title';
+  const imgUrl =
+    root.querySelector('meta[property="og:image"]')?.getAttribute('content') ||
+    'no img';
+  const description =
+    root
+      .querySelector('meta[property="og:description"]')
+      ?.getAttribute('content') || 'no description';
+
+  return { title, imgUrl, description };
 }
